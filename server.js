c206acdb556eb9d8f95870bea8c593df50093d3a
Eroh Oghenemaro Divine <maroeroh@gmail.com>
2026-07-22 23:57:09 +0100

    new server

--- server.js
+++ server.js
@@ -4,13 +4,17 @@ import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 5000;
 
-try {
-  await connectDB();
+const startServer = async () => {
+  try {
+    await connectDB();
 
-  app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-} catch (error) {
-  console.error("STARTUP ERROR:", error);
-  process.exit(1);
-}
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("STARTUP ERROR:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
